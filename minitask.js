366f935b4d5ee9320b9b5a0d390949eaa1020770
Habib Muhammad Rizki <habibboboho@gmail.com>
2024-12-12 13:58:20 +0700

    first commit

---
+++ minitask.js
@@ -0,0 +1,14 @@
+// 1.
+console.log(document.title);
+
+// 2.
+console.log(window.innerWidth, window.innerHeight);
+
+// 3.
+console.log(document.getElementById("title"));
+
+// 4.
+console.log(document.querySelector(".text"));
+
+// 5.
+console.log(document.querySelectorAll(".text"));
